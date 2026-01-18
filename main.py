5faebbc6d4546203455e5778e9655b02fc4cd5ce
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-01-18 14:48:42 +0100

    Added new function to main.py and added new objects to db

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import django_setup
 
-from timetable.models import Teacher, Subject
+from timetable.models import Teacher, Subject, Student, Class
 
 
 #!--Creating teacher and subject
@@ -20,7 +20,31 @@ first_teacher = Teacher.objects.get(id = 1)
 first_subject = Subject.objects.get(id = 1)
 
 #!--Printing first teacher and subject
-print(first_subject)
 print(first_teacher)
+print(first_subject)
 
+#!--Printing related Teacher to Subject
+print(first_subject.teacher)
 
+#!--Creating student and his class
+#first_class = Class.objects.create(
+#    name = "1А",
+#
+#)
+#
+#first_student = Student.objects.create(
+#    name = "Андрій",
+#    student_class = first_class,
+#
+#)
+
+#!--Getting student and class by their id
+first_class = Class.objects.get(id = 1)
+first_student = Student.objects.get(id = 1)
+
+#!--Printing info about class and student
+print(first_class)
+print(first_student)
+
+#!--Printing related Class to Student
+print(first_student.student_class)
